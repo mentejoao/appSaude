c555592895c7b9feb57b9beea3ccc0614c757d73
mentejoao <skhulzxd@gmail.com>
2024-10-05 15:38:30 -0300

    init project

---
+++ app/src/main/java/com/example/appsaude/AvaliacaoFisicaActivity.java
@@ -0,0 +1,12 @@
+package com.example.appsaude;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AvaliacaoFisicaActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_avaliacao); // Crie um arquivo XML correspondente
+    }
+}
